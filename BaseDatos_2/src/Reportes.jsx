18a19c43143b90626d4f4cb7e8433e5ec08dbc86
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-04-11 16:07:44 -0600

    Algunos cambios

--- BaseDatos_2/src/Reportes.jsx
+++ BaseDatos_2/src/Reportes.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Reportes = () => {
   const navigate = useNavigate();
-  const [showDateRange, setShowDateRange] = useState(false);
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [selectedReport, setSelectedReport] = useState(null);
+  const [numOfPeople, setNumOfPeople] = useState('')
 
   const handleSelectReport = (reportNumber) => {
-    if (reportNumber === 1) {
-      setShowDateRange(true);
-    } else {
-      console.log(`Seleccionado reporte número: ${reportNumber}`);
-      // Aquí se podría agregar más lógica dependiendo del reporte seleccionado
-      // navigate(`/reporte-detallado/${reportNumber}`);
-    }
+    setSelectedReport(reportNumber);
+    // Resetear los estados de fecha y cantidad de personas al seleccionar un nuevo reporte
+    setStartDate('');
+    setEndDate('');
+    setNumOfPeople('');
   };
 
   const handleSearchReport = () => {
-    console.log('Buscar reporte con fecha inicial:', startDate, 'y fecha final:', endDate);
+    console.log(
+      "Buscar reporte con fecha inicial:",
+      startDate,
+      "y fecha final:",
+      endDate
+    );
     // Aquí se agregará la lógica para buscar el reporte
     // Esta función podría enviar la solicitud al backend o actualizar el estado para mostrar el reporte
   };
@@ -26,84 +30,147 @@ const Reportes = () => {
   // Estilos para la página
   const styles = {
     container: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '130vh',
-      width: '100vw',
-      backgroundColor: '#ADD8E6',
-      textAlign: 'center',
-      padding: '20px',
-      boxSizing: 'border-box',
+      display: "flex",
+      flexDirection: "column",
+      flexWrap: "wrap",
+      alignItems: "center",
+      justifyContent: "center",
+      minHeight: "100vh",
+      width: "100vw",
+      backgroundColor: "#ADD8E6",
+      textAlign: "center",
+      padding: "20px",
+      boxSizing: "border-box",
+    },
+    buttonsContainer: {
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      margin: "20px 0",
     },
     title: {
-      margin: '20px 0',
-      fontSize: '2.5rem',
-      color: '#333',
+      margin: "20px 0",
+      fontSize: "2.5rem",
+      color: "#333",
     },
     instructionList: {
-      listStyleType: 'none',
+      listStyleType: "none",
       padding: 0,
-      margin: '20px 0',
+      margin: "20px 0",
     },
     instructionItem: {
-      marginBottom: '10px',
+      marginBottom: "10px",
     },
     button: {
-      padding: '10px 20px',
-      margin: '5px', // Un poco de espacio entre botones
-      fontSize: '1.2rem',
-      cursor: 'pointer',
-      borderRadius: '5px',
-      backgroundColor: '#f0f0f0',
+      padding: "10px 20px",
+      margin: "5px", // Un poco de espacio entre botones
+      fontSize: "1rem",
+      cursor: "pointer",
+      borderRadius: "5px",
+      backgroundColor: "#f0f0f0",
+      border: "1px solid #ccc",
+    },
+    input: {
+      padding: '0.5rem',
+      margin: '0.5rem',
       border: '1px solid #ccc',
-      width: '300px', // Ancho fijo para alinear los botones
-    }
+      borderRadius: '5px',
+    },
+    label: {
+      display: 'block',
+      margin: '0.5rem 0',
+    },
   };
 
   const dateRangeStyles = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    margin: '20px 0',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    margin: "20px 0",
   };
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>Reportes</h1>
       <ul style={styles.instructionList}>
-        <li style={styles.instructionItem}>1. Platos más pedidos por los clientes en un rango de fechas.</li>
-        <li style={styles.instructionItem}>2. Horario en el que se ingresan más pedidos entre un rango de fechas.</li>
-        <li style={styles.instructionItem}>3. Promedio de tiempo en que se tardan los clientes en comer, agrupando la cantidad de personas comiendo.</li>
-        <li style={styles.instructionItem}>4. Quejas agrupadas por persona para un rango de fechas.</li>
-        <li style={styles.instructionItem}>5. Quejas agrupadas por plato para un rango de fechas.</li>
-        <li style={styles.instructionItem}>6. Eficiencia de meseros mostrando los resultados de las encuestas, agrupado por personas y por mes para los últimos 6 meses.</li>
+        <li style={styles.instructionItem}>
+          1. Platos más pedidos por los clientes en un rango de fechas.
+        </li>
+        <li style={styles.instructionItem}>
+          2. Horario en el que se ingresan más pedidos entre un rango de fechas.
+        </li>
+        <li style={styles.instructionItem}>
+          3. Promedio de tiempo en que se tardan los clientes en comer,
+          agrupando la cantidad de personas comiendo.
+        </li>
+        <li style={styles.instructionItem}>
+          4. Quejas agrupadas por persona para un rango de fechas.
+        </li>
+        <li style={styles.instructionItem}>
+          5. Quejas agrupadas por plato para un rango de fechas.
+        </li>
+        <li style={styles.instructionItem}>
+          6. Eficiencia de meseros mostrando los resultados de las encuestas,
+          agrupado por personas y por mes para los últimos 6 meses.
+        </li>
       </ul>
-      {[...Array(6).keys()].map((number) => (
-        <button
-          key={number}
-          style={styles.button}
-          onClick={() => handleSelectReport(number + 1)}
-        >
-          Reporte {number + 1}
-        </button>
-      ))}
+      <div style={styles.buttonsContainer}>
+        {[...Array(6).keys()].map((number) => (
+          <button
+            key={number}
+            style={styles.button}
+            onClick={() => handleSelectReport(number + 1)}
+          >
+            Reporte {number + 1}
+          </button>
+        ))}
+      </div>
 
-      {showDateRange && (
+      {[1, 2, 4, 5].includes(selectedReport) && (
         <div style={dateRangeStyles}>
-          <input
-            type="date"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-            style={styles.input}
-          />
-          <input
-            type="date"
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-            style={styles.input}
-          />
+          <h3>{`Fecha para reporte ${selectedReport}`}</h3>
+          <label>
+            Fecha inicial:
+            <input
+              type="date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+              style={styles.input}
+            />
+          </label>
+          <label>
+            Fecha final: 
+            <input
+              type="date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+              style={styles.input}
+            />
+          </label>
+          <button style={styles.button} onClick={handleSearchReport}>
+            Buscar Reporte
+          </button>
+        </div>
+      )}
+
+      {selectedReport === 3 && (
+        <div style={dateRangeStyles}>
+          <h3>Reporte 3: Cantidad de personas</h3>
+          <label>
+            Cantidad de personas:
+            <input
+              type="number"
+              min="1"
+              value={numOfPeople}
+              onChange={(e) => setNumOfPeople(e.target.value)}
+              style={styles.input}
+            />
+          </label>
+          {/* Incluir campos de fecha si es necesario para este reporte */}
+          {/* ... */}
           <button
             style={styles.button}
             onClick={handleSearchReport}
@@ -112,6 +179,10 @@ const Reportes = () => {
           </button>
         </div>
       )}
+
+      {/* Bloque para seleccionar las fechas para ciertos reportes */}
+      {/* ... */}
+
     </div>
   );
 };
